de59a96b1dd0669ecfadfbd160de927d13481f01
Laisses <71999249+Laisses@users.noreply.github.com>
2022-08-18 07:57:19 -0300

    faz o link das imagens com as cartas criadas pelo js

--- script.js
+++ script.js
@@ -1,48 +1,55 @@
 const getsCardsInfo = () => [
-    {frontSrc: "./assets/bobrossparrot.gif", backSrc: "./assets/front.png", cardName: "bobross"},
-    {frontSrc: "./assets/explodyparrot", backSrc: "./assets/front.png", cardName: "explody"},    
-    {frontSrc: "./assets/fiestaparrot", backSrc: "./assets/front.png", cardName: "fiesta"},
-    {frontSrc: "./assets/metalparrot", backSrc: "./assets/front.png", cardName: "metal"},
-    {frontSrc: "./assets/revertitparrot", backSrc: "./assets/front.png", cardName: "revertit"},
-    {frontSrc: "./assets/tripletsparrot", backSrc: "./assets/front.png", cardName: "triplets"},
-    {frontSrc: "./assets/unicornparrot", backSrc: "./assets/front.png", cardName: "unicorn"},
-    {frontSrc: "./assets/bobrossparrot.gif", backSrc: "./assets/front.png", cardName: "bobross"},
-    {frontSrc: "./assets/explodyparrot", backSrc: "./assets/front.png", cardName: "explody"},    
-    {frontSrc: "./assets/fiestaparrot", backSrc: "./assets/front.png", cardName: "fiesta"},
-    {frontSrc: "./assets/metalparrot", backSrc: "./assets/front.png", cardName: "metal"},
-    {frontSrc: "./assets/revertitparrot", backSrc: "./assets/front.png", cardName: "revertit"},
-    {frontSrc: "./assets/tripletsparrot", backSrc: "./assets/front.png", cardName: "triplets"},
-    {frontSrc: "./assets/unicornparrot", backSrc: "./assets/front.png", cardName: "unicorn"},
+    {frontSrc: "./assets/bobrossparrot.gif", cardName: "bobross"},
+    {frontSrc: "./assets/explodyparrot.gif", cardName: "explody"},    
+    {frontSrc: "./assets/fiestaparrot.gif", cardName: "fiesta"},
+    {frontSrc: "./assets/metalparrot.gif", cardName: "metal"},
+    {frontSrc: "./assets/revertitparrot.gif", cardName: "revertit"},
+    {frontSrc: "./assets/tripletsparrot.gif", cardName: "triplets"},
+    {frontSrc: "./assets/unicornparrot.gif", cardName: "unicorn"},
+    {frontSrc: "./assets/bobrossparrot.gif", cardName: "bobross"},
+    {frontSrc: "./assets/explodyparrot.gif", cardName: "explody"},    
+    {frontSrc: "./assets/fiestaparrot.gif", cardName: "fiesta"},
+    {frontSrc: "./assets/metalparrot.gif", cardName: "metal"},
+    {frontSrc: "./assets/revertitparrot.gif", cardName: "revertit"},
+    {frontSrc: "./assets/tripletsparrot.gif", cardName: "triplets"},
+    {frontSrc: "./assets/unicornparrot.gif", cardName: "unicorn"},
 ];
 
 const generateCard = () => {
     const gameBoard = document.querySelector(".game");
-
-    const card = document.createElement("div");
-    const front = document.createElement("div");
-    const frontImg = document.createElement("img");
-    const back = document.createElement("div");
-    const backImg = document.createElement("img");
-
-    card.classList = "card";
-    front.classList = "card__face";
-    back.classList = "card__face";
-    front.classList.add("card__face--front");
-    back.classList.add("card__face--back");
+    const cardsInfo = getsCardsInfo();
     
-    gameBoard.appendChild(card);
-    card.appendChild(front);
-    front.appendChild(frontImg);
-    card.appendChild(back);
-    back.appendChild(backImg);
+    cardsInfo.forEach(item => {
+        const card = document.createElement("div");
+        const front = document.createElement("div");
+        const frontImg = document.createElement("img");
+        const back = document.createElement("div");
+        const backImg = document.createElement("img");
 
-    console.log(gameBoard)
+        card.classList = "card";
+        front.classList = "card__face";
+        back.classList = "card__face";
+        front.classList.add("card__face--front");
+        back.classList.add("card__face--back");
+
+        card.dataset.parrot = item.cardName;
+        frontImg.src = item.frontSrc;
+        backImg.src = "./assets/front.png";
+        
+        front.appendChild(frontImg);
+        back.appendChild(backImg);
+        card.appendChild(front);
+        card.appendChild(back);
+        gameBoard.appendChild(card);
+    });    
+    console.log(gameBoard);
 };
 
-/*const rotateCards = (e) => {    
+generateCard();
+
+const rotateCards = (e) => {    
     const event = e.currentTarget;
     event.classList.toggle("rotate");
-    console.log(event.dataset.parrot);
 }
 
-document.querySelectorAll(".card").forEach(cards => cards.onclick = rotateCards); */
+document.querySelectorAll(".card").forEach(cards => cards.onclick = rotateCards); 
